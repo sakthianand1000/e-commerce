397bc6fa680c3fe43fc96388f6a4b20b9b65b5ba
sakthianand1000 <sakthianand.a1999@gmail.com>
2025-10-05 15:00:22 +0530

    new files added

--- shoppy/Urls.py
+++ shoppy/Urls.py
@@ -3,4 +3,15 @@ from . import views
 
 urlpatterns=[
   path("",views.home,name="home"),
+  path("fav/",views.fav_page,name="fav"),
+  path("login/",views.login_form,name="login"),
+  path("register/",views.register,name="register"),
+  path("form/",views.form,name="form"),
+  path("collection/",views.collection,name="collection"),
+  path("collectionview/",views.collectionview,name="collectionview"),
+  path("fav_page/",views.fav_page,name="fav_page"),
+  path("login_form/",views.login_form,name="login_form"),
+  path("register/",views.register,name="register"),
+  path("form/",views.form,name="form"),
+  path("cart/",views.cart,name="cart")
 ]
--- shoppy/views.py
+++ shoppy/views.py
@@ -1,3 +1,31 @@
+import json
 from django.shortcuts import render
+from .models import Catagory, Products
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    return render(request,"shoppy_prods/index.html")
+
+def form(request):
+    return render(request,"shoppy_prods/form.html")
+
+def collection(request):
+    return render(request,"shoppy_prods/collection.html")
+
+def collectionview(request):
+    return render(request,"shoppy_prods/products/product_index.html")
+
+def fav_page(request):
+    return render(request,"shoppy_prods/fav_page.html")
+
+def login_form(request):
+    return render(request,"shoppy_prods/login_form.html")
+
+def register(request):
+    return render(request,"shoppy_prods/register.html")
+
+def cart(request):
+    return render(request,"shoppy_prods/cart.html")
+
